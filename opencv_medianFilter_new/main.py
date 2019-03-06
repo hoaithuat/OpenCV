089af5e67631cb2680d625ae558f0e0176fd5b4d
hoaithuat <hoaithuat09@gmail.com>
2019-03-06 18:12:40 +0700

    fix component in kernel

--- opencv_medianFilter_new/main.py
+++ opencv_medianFilter_new/main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import cv2 as cv
 
-
-
 img1 = cv.imread('pic1.png', cv.IMREAD_GRAYSCALE)
 height = len(img1)
 width  = len(img1[0])
@@ -10,18 +8,18 @@ filterSize =3
 temp = []
 indexer = filterSize//2
 img2 = np.empty_like(img1)
-for i in range(height-1):
-    for j in range(width-1):
-        for z in range(filterSize):
-            if i+z-indexer < 0 or i+z-indexer > height-2:
-                for tam in range(filterSize):
+for i in range(height):
+    for j in range(width):
+        for rowKernel in range(filterSize):
+            if i+rowKernel-indexer < 0 or i+rowKernel-indexer > height-1:
+                for col1 in range(filterSize):
+                    temp.append(0)
+            elif j+rowKernel-indexer < 0 or j+indexer+rowKernel > width-1:
+                for col2 in range(filterSize):
                     temp.append(0)
             else:
-                if j+z-indexer < 0 or j+indexer > width-2:
-                    temp.append(0)
-                else:
-                    for k in range(filterSize):
-                        temp.append(img1[i+z-indexer][j+k-indexer])
+                for col3 in range(filterSize):
+                    temp.append(img1[i+rowKernel-indexer][j+col3-indexer])
         temp.sort()
         img2[i][j] = temp[len(temp)//2]
         temp = []
